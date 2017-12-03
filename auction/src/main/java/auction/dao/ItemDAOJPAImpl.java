d5d092b31496ce7ebdf2e6f34e794766803dffd1
samdirkx1 <271146@student.fontys.nl>
2017-12-03 17:00:46 +0100

    itemDAO geinitialiseerd item.java persistent gemaakt (als het goed is, het is zo dicht mogelijk dezelfde interpretatie als bij user)

---
+++ auction/src/main/java/auction/dao/ItemDAOJPAImpl.java
@@ -0,0 +1,110 @@
+package auction.dao;
+
+import auction.domain.Item;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+
+public class ItemDAOJPAImpl implements ItemDAO{
+
+    private final EntityManager em;
+
+    public ItemDAOJPAImpl(EntityManager em) {
+        this.em = em;
+    }
+    
+    /**
+     *
+     * @return number of item instances
+     */
+    @Override
+    public int count() {
+        Query q = em.createNamedQuery("Item.count", Item.class);
+        return ((Long) q.getSingleResult()).intValue();
+    }
+
+    /**
+     * The item is persisted. If a item with the same id allready exists an EntityExistsException is thrown
+     * @param item
+     */
+    @Override
+    public void create(Item item) {
+        em.getTransaction().begin();
+        em.persist(item);
+        em.getTransaction().commit();
+    }
+
+    /**
+     * Merge the state of the given item into persistant context. If the item did not exist an IllegalArgumentException is thrown
+     * @param item
+     */
+    @Override
+    public void edit(Item item) {
+        em.getTransaction().begin();
+        em.merge(item);
+        em.getTransaction().commit();
+    }
+
+    /**
+     *
+     * @param id
+     * @return the found entity instance or null if the entity does not exist
+     */
+    @Override
+    public Item find(Long id) {
+        Query q = em.createNamedQuery("Item.findByID", Item.class);
+        q.setParameter("Id", id);
+        try {
+            return (Item) q.getSingleResult();
+        }
+        catch (NoResultException ex) {
+            return null;
+        }
+    }
+    /**
+     *
+     * @return list of item instances
+     */
+    @Override
+    public List<Item> findAll() {
+        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(Item.class));
+        try {
+            return em.createQuery(cq).getResultList();        
+        }
+        catch (NoResultException ex) {
+            return null;
+        }
+
+    }    
+    
+    /**
+     *
+     * @param description 
+     * @return list of item instances having specified description
+     */
+    @Override
+    public List<Item> findByDescription(String description) {
+        Query q = em.createNamedQuery("Item.findByDescription", Item.class);
+        q.setParameter("description", description);
+        try {
+            return (List<Item>) q.getResultList();
+        }
+        catch (NoResultException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Remove the entity instance
+     * @param item - entity instance
+     */
+    @Override
+    public void remove(Item item) {
+        em.getTransaction().begin();
+        em.remove(em.merge(item));
+        em.getTransaction().commit();
+    }
+}
--- auction/src/main/java/auction/domain/Item.java
+++ auction/src/main/java/auction/domain/Item.java
@@ -1,14 +1,31 @@
 package auction.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import nl.fontys.util.Money;
 
-public class Item implements Comparable {
+@Entity
+@NamedQueries({ //implement: verander en add de queries zodat ze passen bij item ipv user
+    @NamedQuery(name = "Item.getAll", query = "select i from Item i"),
+    @NamedQuery(name = "Item.count", query = "select count(i) as itemCount from Item i"),
+    @NamedQuery(name = "Item.findByDescription", query = "select i from Item i where i.description = :description"),
+    @NamedQuery(name = "Item.findById", query = "select i from Item i where i.Id = :Id"),
+})public class Item implements Comparable, Serializable {
 
-    private Long id;
+    @Id @GeneratedValue
+    private Long Id;
     private User seller;
     private Category category;
     private String description;
     private Bid highest;
+    
+    //constructor for JPA manager
+    public Item() {
+    }
 
     public Item(User seller, Category category, String description) {
         this.seller = seller;
@@ -17,7 +34,7 @@ public class Item implements Comparable {
     }
 
     public Long getId() {
-        return id;
+        return Id;
     }
 
     public User getSeller() {
@@ -36,6 +53,32 @@ public class Item implements Comparable {
         return highest;
     }
 
+    //generated met insert code, kijk er maar hoe je het wilt sander
+    public Bid getHighest() {
+        return highest;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setHighest(Bid highest) {
+        this.highest = highest;
+    }
+    //einde insert codes
+    
     public Bid newBid(User buyer, Money amount) {
         if (highest != null && highest.getAmount().compareTo(amount) >= 0) {
             return null;
